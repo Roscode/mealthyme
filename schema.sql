61efe167878d572d548034fdcc6f00a6acf9a945
Darren Roscoe <darrenroscoe@gmail.com>
2017-11-11 02:58:01 -0500

    Add more entity details

--- schema.sql
+++ schema.sql
@@ -2,11 +2,17 @@ use mealthyme;
 
 /*
 Entites:
-Food Item
+Food
+    Need to represent both the concept and an individual food item/amount
+    Maybe foods are the concept and a food item is an amount of food in a pantry
+Food items
+    represent an amount of food in a pantry
+    need to deal with changing the amount e.g. not using all of the rice
 User
     id
     has a pantry (maybe more if you own multiple houses?)
-    as a stretch goal maybe users can share pantries, but that sounds like a tough permissions problem
+    makes meals out of recipes using food items
+    puts food items into pantry
 Meals
     Made up of one or more recipes
         balanced recipes
@@ -16,17 +22,20 @@ Meals
                 derive from food items? promising
         what about leftovers?
             possibly create food item for every recipe called 'leftover-recipe'
-recipes
+Recipes
     contain food items in quanities
     probably taste description and other info i.e. breakfast, lunch, or dinner food
     directions and time info
-pantries
+Pantries
     contains food items
     belongs to a user
     a pantry is basically a table, do we create a table for every pantry? yikes
         maybe pantries is just an id and an owner,
             and there is a pantry-food-item relation table matching items to pantries in quanities,
             and maybe there is a view for each pantry which is the filtered join of the pantries and the p-f-i relation
+Shopping list
+    set of items to buy to make all the meals planned
+    probably a lot more to this, it's like the crux of the application
 */
 
 -- ordered table drops
