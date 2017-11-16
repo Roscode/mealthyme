0bcf9e565266b8ad144caeec078d37aca5f29b32
Darren Roscoe <darrenroscoe@gmail.com>
2017-11-16 02:35:17 -0500

    Remove households, too much complexity for now

--- schema.sql
+++ schema.sql
@@ -8,8 +8,6 @@ drop table if exists recipes;
 drop table if exists shopping_lists;
 drop table if exists pantry_foods;
 drop table if exists pantries;
-drop table if exists user_households;
-drop table if exists households;
 drop table if exists users;
 drop table if exists foods;
 
@@ -29,34 +27,15 @@ CREATE TABLE users (
 
 insert into users (username) values ("roscode");
 
-CREATE TABLE households (
-    household_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    housename VARCHAR(128) NOT NULL
-);
-
-insert into households (housename) values ("roscode's house");
-
-CREATE TABLE user_households (
-    user_id INT NOT NULL,
-    household_id INT NOT NULL,
-    FOREIGN KEY (user_id)
-        REFERENCES users (user_id),
-    FOREIGN KEY (household_id)
-        REFERENCES households (household_id),
-    UNIQUE (user_id , household_id)
-);
-
-insert into user_households values (1, 1);
-
 CREATE TABLE pantries (
     pantry_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    household_id INT NOT NULL,
-    FOREIGN KEY (household_id)
-        REFERENCES households (household_id),
-    UNIQUE (household_id)
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id),
+    UNIQUE (user_id)
 );
 
-insert into pantries (household_id) values (1);
+insert into pantries (user_id) values (1);
 
 CREATE TABLE pantry_foods (
     pantry_id INT NOT NULL,
@@ -70,33 +49,15 @@ CREATE TABLE pantry_foods (
 
 insert into pantry_foods values (1, 1), (1, 2), (1, 3), (1, 4);
 
--- test query: Get all foods in a users pantry
-SELECT 
-    food_id, fname, img_path
-FROM
-    foods
-        JOIN
-    pantry_foods USING (food_id)
-        JOIN
-    pantries USING (pantry_id)
-WHERE
-    household_id = (SELECT 
-            household_id
-        FROM
-            users
-                JOIN
-            user_households USING (user_id)
-        WHERE
-            user_id = 1);
 
 CREATE TABLE shopping_lists (
-    household_id INT NOT NULL,
+    user_id INT NOT NULL,
     food_id INT NOT NULL,
-    FOREIGN KEY (household_id)
-        REFERENCES households (household_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id),
     FOREIGN KEY (food_id)
         REFERENCES foods (food_id),
-    UNIQUE (food_id , household_id)
+    UNIQUE (food_id , user_id)
 );
 
 CREATE TABLE recipes (
