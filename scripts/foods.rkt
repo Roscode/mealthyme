f1f83edf7a1be1244ef8d8ac002bafe78f92ab43
Darren Roscoe <darrenroscoe@gmail.com>
2017-11-28 16:55:57 -0500

    Add script to add foods to database

---
+++ scripts/foods.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(require db)
+
+(define mydb
+  (mysql-connect #:server "localhost"
+                 #:port 3306
+                 #:database "mealthyme"
+                 #:user "mealthyme"))
+
+(call-with-input-file "foods.txt"
+  (lambda (in)
+    (for ([line (in-lines in)])
+      (query-exec mydb "insert into foods (food_name) values (?)" line)))
+  #:mode 'text)
+
+(disconnect mydb)
